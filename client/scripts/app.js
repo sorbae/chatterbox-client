b18120269900013280c18cdb37be501a911d1c1d
Hack Reactor Student <student@hackreactor.com>
2017-10-20 16:25:50 -0700

    Passed all specRunner tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,60 @@
-// YOUR CODE HERE:
+var app = {};
+
+app.init = function () {
+  $('#main').on('click', '.username', function() {
+    app.handleUsernameClick();
+  });
+
+  $('#send .submit').submit(app.handleSubmit);
+};
+
+app.send = function (message) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: message,
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function () {
+  $.ajax({
+    url: undefined,
+    type: 'GET',
+    data: message,
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats > blink').remove();
+};
+
+app.renderMessage = function(message) {
+  $('#chats').html(`<blink>${message.text}</blink>`);
+};
+
+app.renderRoom = function(roomName) {
+  $('#roomSelect').append(`<div>${roomName}</div>`);
+};
+
+app.handleUsernameClick = function() {
+};
+
+app.handleSubmit = function() {
+};
